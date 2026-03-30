09e93930250c4754cc70cffb38afa3c68d64d2a8
skhiri_Mohamed <skhiri.mohamed@isimm.u-monastir.tn>
2026-03-30 11:43:30 +0100

    bbbbb

--- IncidentAPI/Controllers/IncidentsDbController.cs
+++ IncidentAPI/Controllers/IncidentsDbController.cs
@@ -78,7 +78,7 @@ namespace IncidentAPI.Controllers
         public async Task<ActionResult<Incident>> PostIncident(Incident incident)
         {
             incident.CreatedAt = DateTime.UtcNow;
-            incident.Status = "Haaaaaaaaaaaaaaaaaaa";
+            incident.Status = "Hbbbbbbbbbbbbbbbbbbbbbb";
             _context.Incidents.Add(incident);
             await _context.SaveChangesAsync();
 
