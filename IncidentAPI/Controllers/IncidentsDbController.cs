1a945def12855abc3dc3fec8ad4f6036542de03f
skhiri_Mohamed <skhiri.mohamed@isimm.u-monastir.tn>
2026-03-30 12:02:35 +0100

    changed default value of status to open

--- IncidentAPI/Controllers/IncidentsDbController.cs
+++ IncidentAPI/Controllers/IncidentsDbController.cs
@@ -78,7 +78,7 @@ namespace IncidentAPI.Controllers
         public async Task<ActionResult<Incident>> PostIncident(Incident incident)
         {
             incident.CreatedAt = DateTime.UtcNow;
-            incident.Status = "aaaaaa";
+            incident.Status = "OPEN";
             _context.Incidents.Add(incident);
             await _context.SaveChangesAsync();
 
