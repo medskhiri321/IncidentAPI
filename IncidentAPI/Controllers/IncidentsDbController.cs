d38eb5937aac5bbb7204293254d30fdef8e12c00
skhiri_Mohamed <skhiri.mohamed@isimm.u-monastir.tn>
2026-03-30 11:23:52 +0100

    feat : changed behaviour for IncidentsDbController

--- IncidentAPI/Controllers/IncidentsDbController.cs
+++ IncidentAPI/Controllers/IncidentsDbController.cs
@@ -78,7 +78,7 @@ namespace IncidentAPI.Controllers
         public async Task<ActionResult<Incident>> PostIncident(Incident incident)
         {
             incident.CreatedAt = DateTime.UtcNow;
-            incident.Status = "OPEN";
+            incident.Status = "IN PROGRESS";
             _context.Incidents.Add(incident);
             await _context.SaveChangesAsync();
 
